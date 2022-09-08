33544103caca6ac0efe13db08e87eafabe9f0902
POOJARY <Manish.poojary@ad.jktech.com>
2022-09-08 15:52:07 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    //res.status(200).json({title: 'MSAL Node & Express Web App',isAuthenticated: req.session.isAuthenticated,username: req.session.account?.username});
+    res.render('index', {
+        title: 'MSAL Node & Express Web App',
+        isAuthenticated: req.session.isAuthenticated,
+        username: req.session.account?.username,
+    });
+});
+
+module.exports = router;
